5bfb5b6e93396179714dadf01117da97e66f7feb
Tea-Cup <cozyteacup@pm.me>
2025-09-22 07:35:01 +0900

    Darklight wall variants

---
+++ 1.6/Source/Patch_CompDarklightOverlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Foxy.FewTorches {
+	[HarmonyPatch(typeof(CompDarklightOverlay), nameof(CompDarklightOverlay.PostDraw))]
+	public static class Patch_CompDarklightOverlay {
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			foreach (CodeInstruction inst in instructions) {
+				if (inst.LoadsField(AccessTools.Field(typeof(Rot4), nameof(Rot4.North)))) {
+					yield return CodeInstruction.LoadArgument(0);
+					yield return CodeInstruction.LoadField(typeof(ThingComp), nameof(ThingComp.parent));
+					yield return CodeInstruction.Call(typeof(Thing), $"get_{nameof(Thing.Rotation)}");
+				} else {
+					yield return inst;
+				}
+			}
+		}
+	}
+}
