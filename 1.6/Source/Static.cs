417395d203bb38f56dc99a5d13cd7ae7e6457d52
Tea-Cup <cozyteacup@pm.me>
2025-09-24 00:09:11 +0900

    Darklight options for uran and blood lamps

--- 1.6/Source/Static.cs
+++ 1.6/Source/Static.cs
@@ -1,6 +1,12 @@
+using UnityEngine;
+using Verse;
+
 namespace Foxy.FewTorches {
-	[Verse.StaticConstructorOnStartup]
+	[StaticConstructorOnStartup]
 	internal static class Static {
+		public static readonly Texture2D texSetNormalLight = ContentFinder<Texture2D>.Get("UI/Commands/SetNormalLight");
+		public static readonly Texture2D texSetDarklight = ContentFinder<Texture2D>.Get("UI/Commands/SetDarklight");
+
 		static Static() {
 			Logger.Log("Applying patches...");
 			new HarmonyLib.Harmony("Foxy.FewTorches").PatchAll();
